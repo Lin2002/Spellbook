ad06efdc9dcf3d0af511c552de718fe617969fe9
Lin2002 <33724487+Lin2002@users.noreply.github.com>
2018-11-01 09:45:17 -0400

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-$("#revelio").click(function(){
+$(".revelio").click(function(){
     $("#sword").fadeToggle();
     $("#dsword").toggle();
     $("#steve").toggle();
+    $(".aparacium").toggle();
+    $(".expecto").toggle();
 });
 
-$("#aparacium").click(function(){
+$(".aparacium").click(function(){
     $("#shield").fadeToggle();
     $("#wshield").toggle();
     $("#steve").toggle();
-    
+    $(".revelio").toggle();
+    $(".expecto").toggle();
 });
  
     
-$("#expecto").click(function(){
+$(".expecto").click(function(){
     $("#armor").fadeToggle();
     $("#darmor").toggle();
     $("#steve").toggle();
+    $(".revelio").toggle();
+    $(".aparacium").toggle();
 });
     
